edf9d9098d4b933b19dd1dc2e54a9af25f1a575f
Flávia Reis <flaviarster@gmail.com>
2025-05-15 11:25:57 -0300

    style: Botão logout

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,63 +1,71 @@
-import axios from '../service/api';
-import React, { createContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import axios from "../service/api";
+import React, { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        const storedUser = JSON.parse(localStorage.getItem("user"));
-        if(token && storedUser?.id) {
-            fetchUser(storedUser.id, token);
-        } else {
-            setLoading(false);
-        }
-    }, [])
-
-    const login = async (username, password) => {
-        try {
-            const res = await axios.post('https://dummyjson.com/auth/login', {
-                username,
-                password
-            });
-            const token = res.data.accessToken;
-            const userId =res.data.id;
-
-            localStorage.setItem('token', token)
-            await fetchUser(userId, token);
-            navigate("/tasks")
-        } catch (err) {
-            throw new Error('Usuário ou senha inválidos')
-        }
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (token && storedUser?.id) {
+      fetchUser(storedUser.id, token);
+    } else {
+      setLoading(false);
     }
+  }, []);
 
-    const fetchUser = async (id, token) => {
-        try {
-            const userRes = await axios.get(`https://dummyjson.com/users/${id}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                },
-            });
+  const login = async (username, password) => {
+    try {
+      const res = await axios.post("https://dummyjson.com/auth/login", {
+        username,
+        password,
+      });
+      const token = res.data.accessToken;
+      const userId = res.data.id;
 
-            const fullUser = userRes.data;
-
-            localStorage.setItem("user", JSON.stringify(fullUser));
-            setUser(fullUser);
-        } catch (err) {
-            console.log("Erro ao resgatar usuário")
-        } finally {
-            setLoading(false);
-        }
+      localStorage.setItem("token", token);
+      await fetchUser(userId, token);
+      navigate("/tasks");
+    } catch (err) {
+      throw new Error("Usuário ou senha inválidos");
     }
+  };
+
+  const fetchUser = async (id, token) => {
+    try {
+      const userRes = await axios.get(`https://dummyjson.com/users/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const fullUser = userRes.data;
+
+      localStorage.setItem("user", JSON.stringify(fullUser));
+      setUser(fullUser);
+    } catch (err) {
+      console.log("Erro ao resgatar usuário");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    setUser(null);
+    navigate("/");
+  };
 
   return (
-    <AuthContext.Provider value={{user, fetchUser, login, loading}}>
-        {children}
+    <AuthContext.Provider
+      value={{ user, fetchUser, login, loading, logout }}
+    >
+      {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,18 +1,36 @@
+import { useContext } from "react"
 import { TaskForm } from "../components/TaskForm"
 import { TaskList } from "../components/TaskList"
 import { UserSwitcher } from "../components/UserSwitcher"
+import { TaskContext } from "../context/TaskProvider"
+import { Skeleton, Button } from "@mui/material"
+import { AuthContext } from "../context/AuthProvider"
 
 export const TaskPage = () => {
+  const { tasks } = useContext(TaskContext)
+ 
   return (
-    <div className="app-container">
-      <aside className="sidebar">
-        <UserSwitcher />
-      </aside>
-      <main className="main-content">
-        <h1 className="title">TODO List</h1>
-        <TaskForm />
-        <TaskList />
-      </main>
+    <div>
+      <div className="greetings">
+        <p>Olá!</p>
+        <Button variant="contained" type="submit" sx={{ backgroundColor: "#61dafb", color: "black", fontWeight: "bold" }}>
+            Sair
+        </Button>
+      </div>
+      <div className="app-container">
+        <aside className="sidebar">
+          <UserSwitcher />
+        </aside>
+        <main className="main-content">
+          <h1 className="title">TODO List</h1>
+          <TaskForm />
+          { tasks.length === 0 ? Array.from({length: 2}).map((_, i) => (
+            <Skeleton height={50} key={i} />
+          )) :
+          <TaskList />
+          }
+        </main>
+      </div>
     </div>
   )
 }
