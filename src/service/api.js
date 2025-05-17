302dd29b93fd502257440ffae0a899b7299f37c2
flaviare1s <flaviarster@gmail.com>
2025-05-17 15:30:13 -0300

    altera url do backend

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3002"
-})
+  baseURL: "https://usertask-sawv.onrender.com/",
+});
 
 export default instance;
